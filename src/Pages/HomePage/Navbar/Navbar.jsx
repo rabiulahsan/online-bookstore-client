a85f459215c89177eaf087b5d9df6f5c6ebfe7d3
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-19 17:57:37 +0600

    navbar design

--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { LuUserPlus } from "react-icons/lu";
 import { MdOutlineLogout } from "react-icons/md";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import ActiveLink from "../../../Components/ActiveLink/ActiveLink";
 
@@ -12,24 +13,41 @@ const Navbar = () => {
   return (
     <div>
       <div className="hidden md:flex justify-between items-center w-full py-5 px-[6%] relative bg-white bg-opacity-60 backdrop-blur-sm">
-        <div className="title">
-          <Link href="/">
-            <p className="text-4xl font-bold font-playball text-slate-600">
-              <span className="text-rose-500">Book</span>Verse
-            </p>
-          </Link>
-        </div>
-
-        <div className="nav-options flex items-center gap-x-4">
-          <div className="flex items-center gap-x-8 mr-10">
+        <div className="flex">
+          <p className="title">
+            <Link href="/">
+              <p className="text-4xl font-bold font-playball text-slate-600">
+                <span className="text-rose-500">Book</span>Verse
+              </p>
+            </Link>
+          </p>
+          <div className="flex items-center gap-x-6 mx-12">
             <span className="text-slate-500 font-semibold">
               <ActiveLink to="/">Home</ActiveLink>
             </span>
             <span className="text-slate-500 font-semibold">
-              <ActiveLink to="/doctors">Books</ActiveLink>
+              <ActiveLink to="/books">Books</ActiveLink>
             </span>
-          </div>
 
+            {user && (
+              <>
+                <span className="text-slate-500 font-semibold">
+                  <ActiveLink to="/favourites">Favourites</ActiveLink>
+                </span>
+                <span className="text-slate-500 font-semibold">
+                  <ActiveLink to="/my-books">My Books</ActiveLink>
+                </span>
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="nav-options flex items-center gap-x-4">
+          {user && (
+            <span className=" border border-rose-500 rounded-full p-2 bg-rose-500 hover:bg-rose-600 cursor-pointer">
+              <AiOutlineShoppingCart className="text-2xl font-semibold text-white"></AiOutlineShoppingCart>
+            </span>
+          )}
           {user ? (
             <button
               onClick={handleLogOut}
