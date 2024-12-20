3e98b1f57009618f913c9755997f185d80f91738
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-20 15:28:34 +0600

    create verify user hook

---
+++ src/Hooks/useVerifyUser/useVerifyUser.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../useAxiosSecure/useAxiosSecure";
+import useAuth from "../UseAuth/UseAuth";
+
+const useVerifyUser = () => {
+  const { user, loading } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+
+  const [isUser, setIsUser] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (loading || !user?.email) return;
+
+    const verifyUser = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axiosSecure.get(
+          `/api/users/role?email=${user.email}`
+        );
+        setIsUser(res.data.isUser);
+      } catch (error) {
+        console.error("Error verifying instructor:", error);
+        setIsUser(false); // Default to false on error
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    verifyUser();
+  }, [user?.email, loading, axiosSecure]);
+  return [isUser, isLoading];
+};
+
+export default useVerifyUser;
