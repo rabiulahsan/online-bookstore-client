e675148fb16501169eba611b4f8db28a29d4c2c0
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-21 21:41:19 +0600

    add title on favourite page

--- src/Pages/FavouritePage/FavouritePage.jsx
+++ src/Pages/FavouritePage/FavouritePage.jsx
@@ -1,10 +1,23 @@
 import { useLoaderData } from "react-router-dom";
 import useGetFav from "../../Hooks/useGetFav/useGetFav";
+import StaggerAnimation from "../../Components/StaggerAnimation/StaggerAnimation";
 
 const FavouritePage = () => {
   const [favouriteData, isFavLoading] = useGetFav();
   console.log(favouriteData);
-  return <div></div>;
+  return (
+    <div className="px-[4%] bg-slate-100">
+      <div className="flex items-center justify-center py-[2%] gap-x-[2%]">
+        <span className="flex-grow h-[3px] bg-gradient-to-r from-transparent to-rose-500"></span>
+        <span className="font-bold text-3xl mx-4 bg-gradient-to-r from-rose-600 via-pink-500 to-slate-600 bg-clip-text text-transparent">
+          <StaggerAnimation
+            text={"Get Your Favourite Books"}
+          ></StaggerAnimation>
+        </span>
+        <span className="flex-grow h-[3px] bg-gradient-to-r from-rose-600 to-transparent"></span>
+      </div>
+    </div>
+  );
 };
 
 export default FavouritePage;
