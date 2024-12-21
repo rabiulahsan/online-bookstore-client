99987930c7174180d646a1e6d82f67c214c2d083
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-21 19:41:15 +0600

    bookmarks icon changed for adding to favourite

--- src/Pages/BookPage/BookmarkButton.jsx
+++ src/Pages/BookPage/BookmarkButton.jsx
@@ -2,11 +2,21 @@
 import { IoBookmark, IoBookmarkOutline } from "react-icons/io5";
 import useLoggedUser from "../../Hooks/useLoggedUser/useLoggedUser";
 import useAxiosSecure from "../../Hooks/useAxiosSecure/useAxiosSecure";
+import useGetFav from "../../Hooks/useGetFav/useGetFav";
+import { useEffect, useState } from "react";
 
 const BookmarkButton = ({ book }) => {
   //   console.log(book);
   const [axiosSecure] = useAxiosSecure();
   const [loggedUser] = useLoggedUser();
+  const [saved, setSaved] = useState(false);
+  const [favouriteData] = useGetFav();
+
+  // Check if the book is already in favorites
+  useEffect(() => {
+    const isBookSaved = favouriteData.some((data) => data.bookId === book._id);
+    setSaved(isBookSaved);
+  }, [favouriteData, book._id]);
 
   const handleAddingtoFav = async (book) => {
     try {
@@ -16,6 +26,7 @@ const BookmarkButton = ({ book }) => {
           book
         );
         console.log("Bookmark added successfully:", result.data.result);
+        setSaved(true);
       }
     } catch (error) {
       console.error("Error adding bookmark:", error);
@@ -24,13 +35,23 @@ const BookmarkButton = ({ book }) => {
 
   return (
     <div>
-      <p
-        className="bg-rose-100 hover:bg-rose-200 px-5 py-[10px] rounded-sm text-2xl flex items-center justify-center cursor-pointer"
-        title="Add to favourite book"
-        onClick={() => handleAddingtoFav(book)}
-      >
-        <IoBookmarkOutline></IoBookmarkOutline>
-      </p>
+      {saved ? (
+        <p
+          className="bg-rose-100 hover:bg-rose-200 px-5 py-[10px] rounded-sm text-2xl flex items-center justify-center cursor-pointer"
+          title="Add to favourite book"
+          onClick={() => handleAddingtoFav(book)}
+        >
+          <IoBookmark></IoBookmark>
+        </p>
+      ) : (
+        <p
+          className="bg-rose-100 hover:bg-rose-200 px-5 py-[10px] rounded-sm text-2xl flex items-center justify-center cursor-pointer"
+          title="Add to favourite book"
+          onClick={() => handleAddingtoFav(book)}
+        >
+          <IoBookmarkOutline></IoBookmarkOutline>
+        </p>
+      )}
     </div>
   );
 };
