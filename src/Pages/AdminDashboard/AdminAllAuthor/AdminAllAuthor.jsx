224374c9fa1e8edb521212a7c1d01e03a622b167
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-23 22:47:45 +0600

    author deleted

--- src/Pages/AdminDashboard/AdminAllAuthor/AdminAllAuthor.jsx
+++ src/Pages/AdminDashboard/AdminAllAuthor/AdminAllAuthor.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure/useAxiosSecure";
+import { ImBin } from "react-icons/im";
 
 const AdminAllAuthor = () => {
   const [allAuthors, setAllAuthors] = useState([]);
   const [isallAuthorsLoading, setIsallAuthorsLoading] = useState(true);
   const [axiosSecure] = useAxiosSecure();
+  console.log(allAuthors);
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -22,6 +24,27 @@ const AdminAllAuthor = () => {
     fetchBooks();
   }, [axiosSecure]);
 
+  const handleAuthorDelete = async (authorId) => {
+    try {
+      // Call the API to delete the book
+      const response = await axiosSecure.delete(
+        `/api/authors/deleteauthor/${authorId}`
+      );
+      console.log("Delete response:", response.data);
+
+      if (response.status === 200) {
+        // Update the state to reflect the deletion
+        setAllAuthors((prevAuthor) =>
+          prevAuthor.filter((author) => author._id !== authorId)
+        );
+      } else {
+        console.error("Failed to delete the author");
+      }
+    } catch (error) {
+      console.error("Error deleting the author:", error);
+    }
+  };
+
   console.log(allAuthors);
   return (
     <div className="bg-white my-[5%] p-[5%]">
@@ -35,12 +58,26 @@ const AdminAllAuthor = () => {
       ) : (
         <div className="flex flex-col gap-y-5 px-[5%] my-[4%] ">
           {allAuthors?.map((author, i) => (
-            <div className=" bg-slate-100 " key={i}>
+            <div
+              className=" bg-slate-100 flex justify-between items-center px-[5%] py-[2%]"
+              key={i}
+            >
               <img
                 src={author?.image}
                 alt={author?.name}
                 className="w-[40px] h-[40px] rounded-full border-3 border-slate-600 object-cover"
               />
+              <p>{author?.name}</p>
+              <p>{author?.email}</p>
+              <p
+                onClick={() => handleAuthorDelete(author?._id)}
+                className="flex items-center gap-x-2 bg-rose-500 hover:bg-rose-600 text-white font-semibold px-4 py-2 rounded-sm cursor-pointer mt-4"
+              >
+                <span>
+                  <ImBin className="text-xl"></ImBin>
+                </span>
+                Delete
+              </p>
             </div>
           ))}
         </div>
