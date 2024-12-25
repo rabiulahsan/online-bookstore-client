8c1bfd985c585373c05da091e303bcf628e3e8ea
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-25 13:46:43 +0600

    cart page fixed

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,15 +1,38 @@
-import { useContext } from "react";
-import { CartContext } from "../../Providers/CartProvider";
+import { useEffect, useState } from "react";
 import StaggerAnimation from "../../Components/StaggerAnimation/StaggerAnimation";
 import useAxiosSecure from "../../Hooks/useAxiosSecure/useAxiosSecure";
 import useLoggedUser from "../../Hooks/useLoggedUser/useLoggedUser";
 import CartRow from "./CartRow";
 
 const CartPage = () => {
-  const cart = useContext(CartContext);
-  const { cartData, setCartData, isCartLoading } = cart;
   const [axiosSecure] = useAxiosSecure();
   const [loggedUser] = useLoggedUser();
+
+  const [cartData, setCartData] = useState([]);
+  const [isCartLoading, setIsCartLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCarts = async () => {
+      try {
+        setIsCartLoading(true); // Start loading
+        if (loggedUser && loggedUser._id) {
+          const res = await axiosSecure.get(
+            `http://localhost:5000/api/carts/getall/${loggedUser?._id}`
+          );
+          // console.log(res);
+          setCartData(res.data?.items || []);
+          setIsCartLoading(false);
+        }
+      } catch (error) {
+        console.log("Error getting Cart data:", error);
+      }
+    };
+
+    // Only fetch if `loggedUser` is available and stable
+    if (loggedUser && loggedUser._id) {
+      fetchCarts();
+    }
+  }, [loggedUser, axiosSecure]);
 
   const handleItemDelete = async (bookId) => {
     try {
